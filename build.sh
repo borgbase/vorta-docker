1cef2f1c3c6fa886c88cae8de6b4036cd9efc066
MarkLambert <mark.lambert@target.com>
2022-05-06 16:09:18 -0600

    Update to 0.8.4

--- build.sh
+++ build.sh
@@ -5,10 +5,6 @@ MYPID=$$
 renice -n 19 -p $MYPID
 ionice -c 3 -p $MYPID
 
-docker pull jlesage/baseimage-gui:alpine-3.12
+docker pull jlesage/baseimage-gui:alpine-3.15
 # For testing local builds use the local label
 docker build . -t ghcr.io/borgbase/vorta-docker:local
-
-
-
-
