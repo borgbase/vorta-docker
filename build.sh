b250c3701c57f6fac176453cd2cb36442e42526b
Mark Lambert <ranbato@gmail.com>
2024-01-10 15:30:58 -0700

    Update to Vorta 0.9.1

--- build.sh
+++ build.sh
@@ -5,6 +5,7 @@ MYPID=$$
 renice -n 19 -p $MYPID
 ionice -c 3 -p $MYPID
 
-docker pull jlesage/baseimage-gui:alpine-3.17-v4
+docker pull jlesage/baseimage-gui:alpine-3.19-v4
 # For testing local builds use the local label
-docker build . -t ghcr.io/borgbase/vorta-docker:local
+DOCKER_BUILDKIT=1 docker build . -t localhost:5000/vorta-docker:local
+# docker image push localhost:5000/vorta-docker:local
