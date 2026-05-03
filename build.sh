84ae6d98da6e69c771c03824033da2473b92ea63
Mark Lambert <ranbato@gmail.com>
2026-05-03 14:18:35 -0600

    Upgrade to Vorta 0.11.4 usermod no longer available or needed

--- build.sh
+++ build.sh
@@ -7,5 +7,6 @@ ionice -c 3 -p $MYPID
 
 docker pull jlesage/baseimage-gui:alpine-3.22-v4
 # For testing local builds use the local label
+# user --no-cache if you want to force a rebuild of all layers, otherwise it will use the cache for unchanged layers
 DOCKER_BUILDKIT=1 docker build . -t nas:5000/vorta-docker:local
-# docker image push storage:5000/vorta-docker:local
+# docker image push nas:5000/vorta-docker:local
--- rootfs/etc/cont-init.d/50-init.sh
+++ rootfs/etc/cont-init.d/50-init.sh
@@ -5,8 +5,7 @@ mkdir -p /config/xdg/cache
 mkdir -p /config/xdg/data
 mkdir -p /config/.config/autostart
 
-# Configure user home directory
-usermod -d /config app
+
 
 #ls -al /etc/
 #ls -al /etc/cont-init.d/
